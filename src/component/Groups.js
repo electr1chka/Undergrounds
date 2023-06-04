1bcd8e70a3112111ff54c71a291d45f2b1668152
ArtPereima~ <art.pereima@gmail.com>
2023-06-04 20:52:03 +0300

    finished groups

--- src/component/Groups.js
+++ src/component/Groups.js
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet  } from 'react-native';
 import axios from 'axios';
 import { SelectList } from 'react-native-dropdown-select-list'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
 export const Groups = () => {
 
   const [groupsData, setGroupsData] = useState([]);
-  const [selectedGroup, setSelectedGroup] = useState("name");
-
+  const [selectedGroup, setSelectedGroup] = useState('');
   useEffect(() => {
-    fetchData();
-    readSelectedGroup();
+    (async () => {
+      await fetchFromStorage();
+      await fetchData();
+    }
+    )();
   }, []);
-
-  useEffect(() => {
-    storeSelectedGroup();
-  }, [selectedGroup])
-
+  const fetchFromStorage = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@group')
+      if(value !== null) {
+        setSelectedGroup(JSON.parse(value));
+      }
+    } catch(e) {
+      console.log(e)
+    }
+  }
   const fetchData = async() => {
     await axios.get('https://my-json-server.typicode.com/electr1chka/fake-api/groups')
     .then((response) => {
@@ -29,30 +34,19 @@ export const Groups = () => {
     }).catch((err) => { });
   };
 
-  const storeSelectedGroup = async() => {
-    try {
-        const jsonValue = JSON.stringify(selectedGroup)
-        await AsyncStorage.setItem('@group', jsonValue)
-        const data1 = await AsyncStorage.setItem('@group', jsonValue)
-        console.log(jsonValue)
-      } catch (e) {
-        console.log(e);
-      }
-  }
-
-  const readSelectedGroup = async() => {
-    try {
-      const jsonValue = AsyncStorage.getItem('@group')
-      await setSelectedGroup(jsonValue != null ? JSON.parse(jsonValue) : null);
-    } catch(e) {
-      // error reading value
-    }
-  }
 
   return(
-    <SelectList 
-        setSelected={setSelectedGroup} 
-        data={groupsData} 
+    <SelectList
+        setSelected={setSelectedGroup}
+        data={groupsData}
+        onSelect={async ()=>{
+          try{
+            await AsyncStorage.setItem('@group', JSON.stringify(groupsData[selectedGroup-1].value));
+          }
+            catch(e){
+                console.log(e)
+            }
+        }}
     />
   )
 }
