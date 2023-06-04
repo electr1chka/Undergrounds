b80e9efd629d09bba578e92dacf4d05da67e9cec
ArtPereima~ <art.pereima@gmail.com>
2023-06-04 21:29:26 +0300

    Load groups finished

--- src/component/Groups.js
+++ src/component/Groups.js
@@ -5,50 +5,58 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const Groups = () => {
 
-  const [groupsData, setGroupsData] = useState([]);
-  const [selectedGroup, setSelectedGroup] = useState('');
-  useEffect(() => {
-    (async () => {
-      await fetchFromStorage();
-      await fetchData();
-    }
-    )();
-  }, []);
-  const fetchFromStorage = async () => {
-    try {
-      const value = await AsyncStorage.getItem('@group')
-      if(value !== null) {
-        setSelectedGroup(JSON.parse(value));
-      }
-    } catch(e) {
-      console.log(e)
-    }
-  }
-  const fetchData = async() => {
-    await axios.get('https://my-json-server.typicode.com/electr1chka/fake-api/groups')
-    .then((response) => {
-      let newArray = response.data.map((item) => {
-        return {key: item.id, value: item.name}
-      })
-      setGroupsData(newArray);
-    }).catch((err) => { });
-  };
+    const [groupsData, setGroupsData] = useState([]);
+    const [selectedGroup, setSelectedGroup] = useState('');
+    const [defaultGroup, setDefaultGroup] = useState(undefined);
 
+    useEffect(() => {
+        (async () => {
+            await fetchFromStorage();
+            await fetchData();
+        }
+        )();
+    }, []);
 
-  return(
-    <SelectList
-        setSelected={setSelectedGroup}
-        data={groupsData}
-        onSelect={async ()=>{
-          try{
-            await AsyncStorage.setItem('@group', JSON.stringify(groupsData[selectedGroup-1].value));
-          }
-            catch(e){
-                console.log(e)
+    const fetchFromStorage = async () => {
+        try {
+            const value = await AsyncStorage.getItem('@group')
+            if (value !== null) {
+                setDefaultGroup(JSON.parse(value));
             }
-        }}
-    />
-  )
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const fetchData = async () => {
+        await axios.get('https://my-json-server.typicode.com/electr1chka/fake-api/groups')
+            .then((response) => {
+                let newArray = response.data.map((item) => {
+                    return { key: item.id, value: item.name }
+                })
+                setGroupsData(newArray);
+            }).catch((err) => { });
+    };
+
+    const storeData = async () => {
+        try {
+            const group = groupsData[selectedGroup - 1];
+            if(null != group){
+                await AsyncStorage.setItem('@group', JSON.stringify(group));
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    return (
+        <SelectList        
+            setSelected={setSelectedGroup}
+            data={groupsData}
+            defaultOption={defaultGroup}
+            onSelect={async () => storeData()}
+        />
+    )
 }
 
 export default Groups;
--- src/component/Status.js
+++ src/component/Status.js
@@ -3,36 +3,6 @@ import { Image, StyleSheet } from 'react-native';
 import axios from 'axios';
 import { SelectList } from 'react-native-dropdown-select-list'
 
-export const Groups = () => {
-
-  // const [groupsData, setGroupsData] = useState([]);
-  // const [selectedGroup, setSelectedGroup] = useState("");
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
-
-  // const fetchData = () => {
-  //   axios.get('https://my-json-server.typicode.com/electr1chka/fake-api/groups')
-  //   .then((response) => {
-  //     console.log(response);
-  //     /*let newArray = response.data.map((item) => {
-  //       return {key: item.id, value: item.name}
-  //     })
-  //     setGroupsData(newArray);*/
-  //   }).catch((err) => { });
-  // };
-
-  return(
-    // <SelectList 
-    //     setSelected={setSelectedGroup} 
-    //     data={groupsData} 
-    //     onSelect={() => alert(selectedGroup)}
-    // />
-    <></>
-  )
-}
-
 const Status = () => {
   const [statusData, setStatusData] = useState(false);
   useEffect(() => {
